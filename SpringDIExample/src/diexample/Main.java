4328a4d1ff128cb71cd835d69c851ab67096d7b0
vanduongit <vanduongit1706@gmail.com>
2015-03-13 16:42:55 +0700

    Update Main.java

--- SpringDIExample/src/diexample/Main.java
+++ SpringDIExample/src/diexample/Main.java
@@ -6,7 +6,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main {
 	
 	
-	//Duong dai ca da comment tai day
+	//Duong pro da comment tai day
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context=new 
 					ClassPathXmlApplicationContext("di-context.xml");
