55a7961522bd364f94cebf152c0b68e3fce6864b
DuongPV1 <DuongPV1@301DUONGPV1.fsoft.fpt.vn>
2015-03-13 16:43:22 +0700

    Duong super man comment

--- SpringDIExample/src/diexample/Main.java
+++ SpringDIExample/src/diexample/Main.java
@@ -6,7 +6,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main {
 	
 	
-	//Duong dai ca da comment tai day
+	//Duong super man da comment tai day
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context=new 
 					ClassPathXmlApplicationContext("di-context.xml");
