654c60a32f5e63b789db5be49ffaff100a86ef8b
User <User@User-PC>
2015-05-08 01:20:20 +0700

    repair for promote

---
+++ FirstProjectDTT/src/main/java/com/dtweb/dao/PromotionDao.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.dtweb.dao;
+
+import com.dtweb.model.Promotion;
+
+/**
+ * @author User
+ *
+ */
+public interface PromotionDao extends GenericDao<Promotion> {
+
+}
---
+++ FirstProjectDTT/src/main/java/com/dtweb/dao/impl/PromotionDaoImpl.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.dtweb.dao.impl;
+
+import org.springframework.stereotype.Repository;
+
+import com.dtweb.dao.PromotionDao;
+import com.dtweb.model.Promotion;
+
+/**
+ * @author User
+ *
+ */
+
+@Repository
+public class PromotionDaoImpl extends GenericDaoImpl<Promotion> implements PromotionDao {
+
+}
